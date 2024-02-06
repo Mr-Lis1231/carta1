7c103731b6672c3b34a33b6fc4c75683aec4ed6d
tvig <tvigmaster@mail.ru>
2024-02-06 18:29:14 +0300

    номер 2 - доработать

--- main.py
+++ main.py
@@ -1,47 +1,55 @@
 import os
+from PyQt5.QtWidgets import QWidget, QApplication, QLabel
+from PyQt5.QtGui import QPixmap
+from PyQt5.QtCore import Qt
+from PyQt5.QtWidgets import QApplication, QWidget, QPushButton, QLineEdit
 import sys
 from PyQt5 import QtWidgets, QtGui
-import PyQt5
 import requests
 
-map_request = "http://static-maps.yandex.ru/1.x/?ll=30.530887,20.703118&spn=100,5.2&l=map"
-response = requests.get(map_request)
-
-if not response:
-    print("Ошибка выполнения запроса:")
-    print(map_request)
-    print("Http статус:", response.status_code, "(", response.reason, ")")
-    sys.exit(1)
-
-# Запишем полученное изображение в файл.
-map_file = "map.png"
-with open(map_file, "wb") as file:
-    file.write(response.content)
-
-# Инициализируем pygame
 
 
-
-
-
-import sys
-
-from PyQt5.QtWidgets import QApplication, QWidget, QPushButton, QLineEdit
 
 
 class WordTrick(QWidget):
-
     def __init__(self):
         super().__init__()
+        self.x = 70
+        self.map()
 
         self.setGeometry(800, 800, 800, 800)
         self.setWindowTitle('карта')
-        self.lbl = QtWidgets.QLabel(self)
-        self.pix = QtGui.QPixmap('map.png')
-        self.lbl.setPixmap(self.pix)
-        self.lbl.move(100, 200)
+        self.pix = QLabel(self)
+        self.pix.setPixmap(QPixmap('map.png'))
+        self.pix.move(0, 0)
+    def keyPressEvent(self, event):
+        if event.key() == Qt.Key_Up:
+            self.x -= 5
+            self.map()
+            self.map_update()
+        elif event.key() == Qt.Key_Down:
+            self.x += 5
+            self.map()
+            print('down', self.x)
+            self.map_update()
 
+    def map_update(self):
+        self.pix.setPixmap(QPixmap('map.png'))
+        self.pix.move(0, 0)
 
+    def map(self):
+        map_request = f"http://static-maps.yandex.ru/1.x/?ll=30.530887,20.703118&spn={self.x},5.2&l=map"
+        response = requests.get(map_request)
+
+        if not response:
+            print("Ошибка выполнения запроса:")
+            print(map_request)
+            print("Http статус:", response.status_code, "(", response.reason, ")")
+            sys.exit(1)
+
+        map_file = "map.png"
+        with open(map_file, "wb") as file:
+            file.write(response.content)
 
 
 if __name__ == '__main__':
