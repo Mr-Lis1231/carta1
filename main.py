92026519188b85a2363a1d824a6e2c2edc1c7534
tvig <tvigmaster@mail.ru>
2024-02-06 17:30:51 +0300

    номер 1

--- main.py
+++ main.py
@@ -1,16 +1,51 @@
-# This is a sample Python script.
+import os
+import sys
+from PyQt5 import QtWidgets, QtGui
+import PyQt5
+import requests
 
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+map_request = "http://static-maps.yandex.ru/1.x/?ll=30.530887,20.703118&spn=100,5.2&l=map"
+response = requests.get(map_request)
+
+if not response:
+    print("Ошибка выполнения запроса:")
+    print(map_request)
+    print("Http статус:", response.status_code, "(", response.reason, ")")
+    sys.exit(1)
+
+# Запишем полученное изображение в файл.
+map_file = "map.png"
+with open(map_file, "wb") as file:
+    file.write(response.content)
+
+# Инициализируем pygame
 
 
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+8 to toggle the breakpoint.
 
 
-# Press the green button in the gutter to run the script.
+
+import sys
+
+from PyQt5.QtWidgets import QApplication, QWidget, QPushButton, QLineEdit
+
+
+class WordTrick(QWidget):
+
+    def __init__(self):
+        super().__init__()
+
+        self.setGeometry(800, 800, 800, 800)
+        self.setWindowTitle('карта')
+        self.lbl = QtWidgets.QLabel(self)
+        self.pix = QtGui.QPixmap('map.png')
+        self.lbl.setPixmap(self.pix)
+        self.lbl.move(100, 200)
+
+
+
+
 if __name__ == '__main__':
-    print_hi('PyCharm')
-
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+    app = QApplication(sys.argv)
+    ex = WordTrick()
+    ex.show()
+    sys.exit(app.exec())
